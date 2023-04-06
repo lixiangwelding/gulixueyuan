da3ae392c49afa993252e3861ba6ec47063db09b
LiXiang <993215231@qq.com>
2023-04-06 19:18:05 +0800

    day2--统一全局异常处理完全完成

--- service/service_edu/src/main/java/com/atguigu/eduservice/controller/EduTeacherController.java
+++ service/service_edu/src/main/java/com/atguigu/eduservice/controller/EduTeacherController.java
@@ -1,9 +1,23 @@
 package com.atguigu.eduservice.controller;
 
+import com.atguigu.commonutils.R;
+import com.atguigu.eduservice.entity.EduTeacher;
+import com.atguigu.eduservice.entity.vo.TeacherQuery;
+import com.atguigu.eduservice.service.EduTeacherService;
+import com.atguigu.servicebase.exceptionhandler.GuliException;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import org.springframework.web.bind.annotation.RestController;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * <p>
@@ -13,9 +27,114 @@ import org.springframework.web.bind.annotation.RestController;
  * @author testjava
  * @since 2023-04-06
  */
+@Api(description = "讲师管理")
 @RestController
-@RequestMapping("/eduservice/edu-teacher")
+@RequestMapping("/eduservice/teacher")
 public class EduTeacherController {
+    @Autowired
+    private EduTeacherService teacherService;
 
+    //@ApiOperation(value = “接口说明”, httpMethod = “接口请求方式”, response =
+    //“接口返回参数类型”, notes = “接口发布说明”；其他参数可参考源码；
+    //1 查询讲师表所有数据 rest风格
+    @ApiOperation(value = "所有讲师列表")
+    @GetMapping("findAll")
+    public R findAllTeacher(){
+        try {
+            int a = 10/0;
+        }catch(Exception e) {
+            throw new GuliException(20001,"出现自定义异常");
+        }
+        List<EduTeacher> list = teacherService.list(null);
+        return R.ok().data("items",list);
+    }
+
+    //逻辑删除配合mybatis-plus提供的插件 com.atguigu.eduservice.config里面
+    // 逻辑删除
+    @DeleteMapping("{id}")
+    @ApiOperation(value = "逻辑删除讲师")
+    public R removeTeacher(@ApiParam(name = "id",value = "讲师id",required = true)
+            @PathVariable String id){
+        boolean flag = teacherService.removeById(id);
+        if (flag){
+            return R.ok();
+        }else {
+            return R.error();
+        }
+    }
+    //新增老师
+    @PostMapping("addTeacher")
+    public R addTeacher(@RequestBody EduTeacher eduTeacher){
+        boolean save = teacherService.save(eduTeacher);
+        if(save){
+            return R.ok();
+        }else {
+            return R.error();
+        }
+    }
+    //修改老师
+    @PostMapping("updateTeacher")
+    public R updateTeacher(@RequestBody EduTeacher eduTeacher){
+        boolean save = teacherService.updateById(eduTeacher);
+        if(save){
+            return R.ok();
+        }else {
+            return R.error();
+        }
+    }
+    //根据老师id进行查询
+    @GetMapping("getTeacher/{id}")
+    public R getTeacher(@PathVariable String id){
+        EduTeacher eduTeacher = teacherService.getById(id);
+        return R.ok().data("teacher",eduTeacher);
+    }
+    // 分页查询讲师的方法
+    @GetMapping("pageTeacherCondition/{current}/{limit}")
+    public R pageListTeacher(
+            @ApiParam(name = "current",value = "当前页码",required = true)
+            @PathVariable long current,
+            @ApiParam(name = "limit",value = "每页记录数",required = true)
+            @PathVariable long limit)
+
+    {
+        Page<EduTeacher> pageTeacher = new Page<>(current,limit);
+        teacherService.page(pageTeacher,null);
+        long total = pageTeacher.getTotal();
+        List<EduTeacher> records = pageTeacher.getRecords();
+        //第二种方法
+        Map map = new HashMap();
+        map.put("total",total);
+        map.put("rows",records);
+        return R.ok().data(map);
+
+//        return R.ok().data("total",total).data("rows",records);
+    }
+    // 条件查询带分页的方法
+    @PostMapping("pageTeacherCondition/{current}/{limit}")
+    public R pageTeacherCondition(@PathVariable long current,@PathVariable long limit,
+                                  @RequestBody(required = false) TeacherQuery teacherQuery){
+        Page<EduTeacher> pageTeacher = new Page<>(current,limit);
+        LambdaQueryWrapper<EduTeacher> lambdaQueryWrapper = new LambdaQueryWrapper<>();
+        String name = teacherQuery.getName();
+        Integer level = teacherQuery.getLevel();
+        String begin = teacherQuery.getBegin();
+        String end = teacherQuery.getEnd();
+        if(!StringUtils.isEmpty(name)){
+           lambdaQueryWrapper.like(EduTeacher::getName,name);
+        }
+        if(!StringUtils.isEmpty(level)){
+            lambdaQueryWrapper.eq(EduTeacher::getLevel,level);
+        }
+        if(!StringUtils.isEmpty(begin)){
+            lambdaQueryWrapper.ge(EduTeacher::getGmtCreate,begin);
+        }
+        if(!StringUtils.isEmpty(end)){
+            lambdaQueryWrapper.le(EduTeacher::getGmtCreate,end);
+        }
+        teacherService.page(pageTeacher,lambdaQueryWrapper);
+        long total = pageTeacher.getTotal();
+        List<EduTeacher> records = pageTeacher.getRecords();
+        return R.ok().data("total",total).data("rows",records);
+    }
 }
 
