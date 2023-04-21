0918fa80a067bb9a762c360d417da3711a65a69a
LiXiang <993215231@qq.com>
2023-04-21 16:09:29 +0800

    day7 树形结构完成

--- service/service_edu/src/main/java/com/atguigu/eduservice/controller/EduSubjectController.java
+++ service/service_edu/src/main/java/com/atguigu/eduservice/controller/EduSubjectController.java
@@ -2,14 +2,14 @@ package com.atguigu.eduservice.controller;
 
 
 import com.atguigu.commonutils.R;
+import com.atguigu.eduservice.entity.subject.OneSubject;
 import com.atguigu.eduservice.service.EduSubjectService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 /**
  * <p>
@@ -33,6 +33,12 @@ public class EduSubjectController {
         //上传过来excel文件
         subjectService.saveSubject(file,subjectService);
         return R.ok();
+    }
+
+    @GetMapping("")
+    public R getAllSubject(){
+        List<OneSubject> allOneTwoSubject = subjectService.getAllOneTwoSubject();
+        return  R.ok().data("items",allOneTwoSubject);
     }
 }
 
--- service/service_edu/src/main/java/com/atguigu/eduservice/service/impl/EduSubjectServiceImpl.java
+++ service/service_edu/src/main/java/com/atguigu/eduservice/service/impl/EduSubjectServiceImpl.java
@@ -3,16 +3,21 @@ package com.atguigu.eduservice.service.impl;
 import com.alibaba.excel.EasyExcel;
 import com.atguigu.eduservice.entity.EduSubject;
 import com.atguigu.eduservice.entity.excel.SubjectData;
+import com.atguigu.eduservice.entity.subject.OneSubject;
+import com.atguigu.eduservice.entity.subject.TwoSubject;
 import com.atguigu.eduservice.listener.SubjectExcelListener;
 import com.atguigu.eduservice.mapper.EduSubjectMapper;
 import com.atguigu.eduservice.service.EduSubjectService;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * <p>
@@ -36,4 +41,41 @@ public class EduSubjectServiceImpl extends ServiceImpl<EduSubjectMapper, EduSubj
             throw new RuntimeException(e);
         }
     }
+
+    @Override
+    public List<OneSubject> getAllOneTwoSubject() {
+        //最终要得到的列表
+        ArrayList<OneSubject> finalOneTwoTSubject = new ArrayList<>();
+
+        //查询所有一级标题
+        LambdaQueryWrapper<EduSubject> lambdaQueryWrapper = new LambdaQueryWrapper<>();
+        lambdaQueryWrapper.eq(EduSubject::getParentId,"0");
+        List<EduSubject> oneSubjectList = baseMapper.selectList(lambdaQueryWrapper);
+
+        //查询所有二级标题
+        LambdaQueryWrapper<EduSubject> lambdaQueryWrapper2 = new LambdaQueryWrapper<>();
+        lambdaQueryWrapper2.ne(EduSubject::getParentId,"0");
+        List<EduSubject> twoSubjectList = baseMapper.selectList(lambdaQueryWrapper2);
+
+        for (EduSubject eduSubject : oneSubjectList) {
+           OneSubject oneSubject = new OneSubject();
+            BeanUtils.copyProperties(eduSubject,oneSubject);
+            finalOneTwoTSubject.add(oneSubject);
+
+            List<TwoSubject> finalTwoSubjectList = new ArrayList<>();
+            for (EduSubject eduSubject2 : twoSubjectList) {
+                if(eduSubject2.getParentId().equals(oneSubject.getId())){
+                    TwoSubject twoSubject = new TwoSubject();
+                    BeanUtils.copyProperties(eduSubject2,twoSubject);
+                    finalTwoSubjectList.add(twoSubject);
+                }
+            }
+            //把一级下面的所有二级分类放到一级分类里面
+            oneSubject.setSubjectChildren(finalTwoSubjectList);
+        }
+
+
+        return finalOneTwoTSubject;
+    }
+
 }
